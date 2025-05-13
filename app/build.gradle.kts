de8c9fcaab7a9dacef78dea82fa81aee6dc8630f
Catelt <hovanduy0501@gmail.com>
2025-05-13 16:54:42 +0700

    chore: Bump 1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.catelt.quicklink"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.2"
+        versionCode = 100
+        versionName = "1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
